e628b508ec10ae5f87daec226045cf27e8363b40
Shivam Mehta <shivam.mehta007@gmail.com>
2022-08-30 11:21:13 +0200

    Adding tests for all modules in code

--- src/model/HMMComponents/EmissionModel.py
+++ src/model/HMMComponents/EmissionModel.py
@@ -1,11 +1,12 @@
+import torch
 import torch.distributions as tdist
 import torch.nn as nn
-import torch
 
 
 class EmissionModel(nn.Module):
     r"""
-    Emission Model of the HMM, it represents the probability of emitting an observation based on the current state
+    Emission Model of the HMM, it represents the probability of
+        emitting an observation based on the current state
     """
 
     def __init__(self):
@@ -20,19 +21,24 @@ class EmissionModel(nn.Module):
 
     def forward(self, x_t, means, stds, state_lengths):
         r"""
-        Calculates the log probability of the the given data (x_t) being observed from states
+        Calculates the log probability of the the given data (x_t)
+            being observed from states
         Args:
              x_t (float tensor) : observation at current time step
                 shape: (batch, feature_dim)
              means (float tensor): means of the distributions of hidden states
                 shape: (batch, hidden_state, feature_dim)
-             stds (float tensor): standard deviations of the distributions of the hidden states
-                shape: (feature_dim)     tdist.normal.Normal will broadcast to the shape needed
+             stds (float tensor): standard deviations of the
+                                    distributions of the hidden states
+                shape: (batch, hidden_state, feature_dim or feature_dim)
+                                tdist.normal.Normal will broadcast
+                                                to the shape needed
             state_lengths (int tensor): Lengths of states in a batch
                 shape: (batch)
 
         Returns:
-            out (float tensor): observation log likelihoods, expressing the probability of an observation
+            out (float tensor): observation log likelihoods,
+                                    expressing the probability of an observation
                 being generated from a state i
                 shape: (batch, hidden_state)
         """
@@ -46,8 +52,9 @@ class EmissionModel(nn.Module):
 
         mask_tensor = x_t.new_zeros(T_max)
         state_lengths_mask = (
-            torch.arange(T_max, out=mask_tensor).expand(len(state_lengths), T_max) < (state_lengths).unsqueeze(
-                1)).unsqueeze(2)
+            torch.arange(T_max, out=mask_tensor).expand(len(state_lengths), T_max)
+            < (state_lengths).unsqueeze(1)
+        ).unsqueeze(2)
 
         out = torch.sum(out * state_lengths_mask, dim=2)
 
